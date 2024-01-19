7b3c6272ac54f43fc6f3501186234b7fcd169c67
Abhishek Ranjan <ranjan3324@gmail.com>
2024-01-19 09:44:32 +0530

    intial commit

---
+++ admin/includes/db.php
@@ -0,0 +1,6 @@
+<?php 
+	$db = new mysqli("localhost", "root", "", "tbms_db");
+	if($db->connect_errno) {
+		echo "PLEASE BEAR WITH US AS WE ARE CURRENTLY WORKING ON OUR SITE!!!! PLEASE COME BACK LATER";	
+	}
+?>
